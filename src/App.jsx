5d66d15535606758d4acabaaba86608c4bb1e09c
ROHIT KUMAR <rohit9909mr@gmail.com>
2022-01-11 15:51:42 +0530

    todo file added

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
 
 import './App.css';
-import { useSelector } from "react-redux"
+import { useSelector, useDispatch } from "react-redux"
+import { addTodo } from "./store/actions"
 
 
 function App() {
     const counter = useSelector((state) => state.counter)
+    const dispatch = useDispatch();
   return (
     <div className="App">
         <h1>Counter: {counter}</h1>
+        <button 
+        onClick={() => {
+
+        }}
+        >
+            ADD_1
+        </button>
     </div>
   );
 }
---
+++ src/components/Todos.jsx
@@ -0,0 +1,20 @@
+import { useState } from "react"
+
+export const Todos = () => {
+
+    const [text, setText] = useState("")
+    return (
+        <div>
+            <input
+             value={text}
+             type="text" 
+             placeholder="Enter ToDO"
+             onChange={(e) => setText(e.target.value)} 
+             />
+             <button onClick={() => {
+                 //
+             }}>Add Todo</button>
+             {}
+        </div>
+    );
+};
