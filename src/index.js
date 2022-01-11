49a55a3703595617f4bfb3d94083424c4ec31764
ROHIT KUMAR <rohit9909mr@gmail.com>
2022-01-11 15:28:08 +0530

    importing redux

--- src/index.js
+++ src/index.js
@@ -3,10 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {Provider as ReduxProvider } from "react-redux"
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ReduxProvider>
+      <App />
+
+    </ReduxProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
