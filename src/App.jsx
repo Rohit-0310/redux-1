c06ef7639c82a56d707d107ab0876e01004b8a6c
ROHIT KUMAR <rohit9909mr@gmail.com>
2022-01-13 15:34:48 +0530

    Counter Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import "./App.css";
+import { Counter } from "./componants/Counter";
 import { TodoInput } from "./componants/Todoinput";
 
 function App() {
   return (
     <div className="App">
+      <Counter />
       <h1>Todo List</h1>
       <TodoInput />
     </div>
--- src/store/store.js
+++ src/store/store.js
@@ -1,4 +1,13 @@
-import { createStore } from "redux";
-import { reducer } from "../features/Todos/reducer"; 
+import { createStore, combineReducers} from "redux";
+import {  reducer as TodosReducer  } from "../features/Todos/reducer"; 
+import { reducer as CounterReducer } from "../features/Counter/reducer";
 
-export const store = createStore(reducer);
+// export const store = createStore(reducer);
+
+
+let rootReducer=combineReducers({
+    todosState:TodosReducer,
+    counterState:CounterReducer
+})
+export const store=createStore(rootReducer,window.__REDUX_DEVTOOLS_EXTENSION__());
+console.log(store.getState());
